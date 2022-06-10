b268740a4df0b72c4e82bd806d8c3c958ba8e0ee
AdrianWojtowicz <adrian.wojtowicz@op.pl>
2022-06-10 21:25:10 +0200

    app class to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Modal from "./Modal/Modal";
 import Searchbar from "./Searchbar/Searchbar";
 import Button from "./Button/Button";
@@ -7,42 +7,31 @@ import Loader from "./Loader/Loader";
 import { Api } from "./Api/Api";
 import "./App.css";
 
-export class App extends Component {
-  state = {
-    pictures: [],
-    page: 1,
-    query: "",
-    largeImage: "",
-    imgTags: "",
-    error: null,
-    showModal: false,
-    loading: false,
-    finish: false,
-  };
+const initialState = {
+  pictures: [],
+  largeImage: "",
+  imgTags: "",
+  error: false,
+  showModal: false,
+  loading: false,
+  finish: false,
+};
 
-  componentDidUpdate(prevProps, prevState) {
-    const { query, loading, page } = this.state;
-    if (prevState.query !== query || (loading && prevState.page < page)) {
-      this.fetchPictures();
-    }
-  }
+export const App = () => {
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+  const [state, setState] = useState(initialState);
 
-  toggleModal = () => {
-    this.setState((state) => ({
-      showModal: !state.showModal,
-    }));
-  };
-  
-  async fetchPictures() {
+  const { pictures, loading, error, showModal, largeImage, imgTags, finish } = state;
+
+  const fetchPictures = async () => {
     try {
-      const { page, query } = this.state;
-      this.setState({ loading: true });
       const { data } = await Api.searchImages(page, query);
-      this.setState(({ pictures }) => {
+      setState(({ pictures }) => {
         const newState = {
           pictures: [...pictures, ...data.hits],
           loading: false,
-          error: null,
+          error: false,
         };
         if (data.hits.length < 11) {
           newState.finish = true;
@@ -53,47 +42,67 @@ export class App extends Component {
         return newState;
       });
     } catch (error) {
-      this.setState({
+      setState({
         loading: false,
-        error,
+        error: false,
       });
     }
   }
 
-  handleOpenModal = (largeImage = "") => {
-    this.setState({ largeImage });
-    this.toggleModal();
-  };
-
-  onChangeQwery = ({ query }) => {
-    this.setState({ query: query, page: 1, pictures: [], error: null });
-  };
-
-  loadMore = () => {
-    this.setState(({ page }) => ({
-      loading: true,
-      page: page + 1,
-    }));
-  };
-
-  render() {
-    const { pictures, loading, error, showModal, largeImage, imgTags, finish } =
-      this.state;
-    return (
-      <div className="App">
-        <Searchbar onSubmit={this.onChangeQwery} />
-        {error && <h1>Impossible to load the pictures!</h1>}
-        {!error && (
-          <ImageGallery pictures={pictures} onClick={this.handleOpenModal} />
-        )}
-        {!finish && pictures.length !== 0 && <Button onClick={this.loadMore} />}
-        {loading && <Loader />}
-        {showModal && (
-          <Modal showModal={this.handleOpenModal}>
-            <img src={largeImage} alt={imgTags} />
-          </Modal>
-        )}
-      </div>
-    );
+  const handleOpenModal = (largeImage) => {
+    setState({
+      ...state,
+      largeImage,
+      showModal: true,
+    })
   }
+
+  const onChangeQuery = (query) => {
+    setQuery(query);
+  }
+
+  const loadMore = () => {
+    setPage((prevState) => {
+     return prevState + 1
+    });
+    state.loading = true;
+    setState({ ...state });
+  }
+
+  const closeModal = () => {
+    setState(({ showModal }) => {
+      return {
+        ...state,
+        showModal: !showModal,
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (!query) return;
+    setState({ pictures: [], isLoading: true, finish: false });
+    fetchPictures();
+  }, [query])
+
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+    fetchPictures();
+  }, [page])
+
+  return (
+    <div className="App">
+      <Searchbar onSubmit={onChangeQuery} />
+      {error && <h1>Impossible to load the pictures!</h1>}
+      {!error && <ImageGallery pictures={pictures} onClick={handleOpenModal} />}
+      {!finish && pictures.length !== 0 && <Button onClick={loadMore} />}
+      {loading && <Loader />}
+      {showModal && (
+        <Modal showModal={closeModal}>
+          <img src={largeImage} alt={imgTags} />
+        </Modal>
+      )}
+    </div>
+  )
 }
